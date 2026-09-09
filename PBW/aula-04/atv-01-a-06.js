be0b9851d1abf99188b6080cb6e23013a7ed8f47
Thiago Minuzzi <tmb.minuzzi@gmail.com>
2026-09-09 10:15:29 -0300

    Atividades da aula 04

---
+++ PBW/aula-04/atv-01-a-06.js
@@ -0,0 +1,43 @@
+//1
+/*for (let i = 1; i <= 10; i++) {
+    console.log(i)
+}*/
+
+//2
+/*for (let i = 10; i >= 1; i--) {
+    console.log(i)
+}*/
+
+//3
+/*for (let i = 0; i <= 20; i+= 2) {
+    console.log(i)
+}*/
+
+//4
+/*for (let i = 1; i <= 19; i+= 2) {
+    console.log(i)
+}*/
+
+//5
+
+/*let soma = 0
+
+for (let i = 0; i <= 5; i++) {
+    soma += i
+}
+
+console.log(soma)*/
+
+//6
+/*let soma = 0
+
+for (let i = 1; i <= 100; i++) {
+    soma += i
+}
+
+console.log(soma)*/
+
+
+
+
+
